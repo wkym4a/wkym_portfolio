295c05c52c72390372ebf0788bb6c4d340cff965
若山侑輝 <wkym_4a@icloud.com>
2020-05-03 23:26:09 +0900

    大まかな表示まで、レイアウトはまだ

--- src/portfolio/ContentForm.js
+++ src/portfolio/ContentForm.js
@@ -4,7 +4,9 @@ import { beforePortfolio } from './Store';
 import { nextPortfolio } from './Store';
 import { changeMemo } from './Store';
 
-import Icon from '../img/Img3.png'
+import Icon1 from '../img/Img1.jpeg'
+import Icon2 from '../img/Img2.png'
+import Icon3 from '../img/Img3.png'
 
 class ContentForm extends Component {
 
@@ -12,6 +14,17 @@ class ContentForm extends Component {
     fontSize:"14pt",
     color:"#006",
     padding:"0px",
+  }
+  icon_flame = {
+    width:"800px",
+    higth:"400px",
+    backgroundColor:"red"
+  }
+  icon_content = {
+    // position: "absolute",
+    width:"100%",
+    higth:"100%",
+    objectFit: "cover"
   }
 
   constructor(props){
@@ -45,19 +58,38 @@ class ContentForm extends Component {
   }
 
   render(){
+    let port_name
+    let url_info
+    let poto_info
+
+
+    switch (this.props.num) {
+      case 0:
+        port_name= "Maっぷら";
+        url_info= "http://mappura.com"
+        poto_info = Icon1
+        break;
+      case 1:
+        port_name= "八千代台の不動産屋が作った月極駐車場管理アプリ";
+        url_info= "http://yachiyo-parking.herokuapp.com"
+        poto_info = Icon2
+        break;
+      case 2:
+        port_name= "タスク管理システム(社内使用を想定)";
+        url_info= "https://safe-chamber-93891.herokuapp.com"
+        poto_info = Icon3
+        break;
+    }
+
     return (
       <div>
-        内容情報{this.props.num}
-        <div>
-          ストアから<input type="text" size="80" onChange={this.doChange}
-          style={this.input}  value={this.props.data[this.props.num].memo} />
-        </div>
-        
+        <h2>【{port_name}】</h2>
+        <a>{this.props.message}</a>
+        <a href={url_info}>リンク</a>
+        {/* <a type="submit"  value="リンク" href={url_info}/> */}
         <input type="submit"  value="前へ" onClick={this.doBefore}/>
+        <div style={this.icon_flame}><img src={poto_info}  alt="アイコン" style={this.icon_content}/></div>
         <input type="submit"  value="次へ" onClick={this.doNext}/>
-
-        <img src={Icon}  alt="アイコン" />
-
       </div>
     );
   }
--- src/portfolio/HeadForm.js
+++ src/portfolio/HeadForm.js
@@ -9,11 +9,55 @@ class HeadForm extends Component {
   }
 
   render(){
-    return (
-      <div >
-        ヘッド情報{this.props.num}
-      </div>
-    );
+    // return <div>ヘッド情報</div>
+    let port1="● Maっぷら"
+    let port2="● 八千代台の不動産屋が作った月極駐車場管理アプリ"
+    let port3="● タスク管理システム(社内使用を想定)"
+    let select_port1= "　 "
+    let select_port2= "　 "
+    let select_port3= "　 "
+    
+    switch (this.props.num) {
+      case 0:
+        select_port1= "=>"
+        break;
+      case 1:
+        select_port2= "=>"
+        break;
+      case 2:
+        select_port3= "=>"
+        break;
+    }
+
+    return (<div>
+      <div>{select_port1}{port1}</div>
+      <div>{select_port2}{port2}</div>
+      <div>{select_port3}{port3}</div>
+      </div>)
+    // <div>ヘッド情報vv{num}</div>
+
+
+    // if (this.props.num===1){
+    //   return <div>{this.props.num}</div>
+    // }else{
+    //   return <div>ヘッド情報</div> 
+    // }
+    // return (
+    //   <div >
+    //     {if (this.props.num===1){
+    //       <div>1111</div>
+    //     }}
+    //     {/* switch (this.props.num){
+    //       case '0':
+    //         <div></div>
+    //       break;
+
+    //       default:
+    //         <div></div>
+    //     } */}
+    //     ヘッド情報{this.props.num}
+    //   </div>
+    // );
   }
 
 }
